27c396973fb1829e43946b60d212e737a9766f1c
B S S Sathvik <84376499+BSSSATHVIK@users.noreply.github.com>
2026-03-13 18:52:11 +0530

    Update streamlit_app.py
    Updated errors

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,6 @@ if ingredients_list:
         session.sql(insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {order_name}!', icon="✅")
 
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 st.text(smoothiefroot_response)
     
